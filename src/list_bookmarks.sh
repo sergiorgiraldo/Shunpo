9f71ec5fc709ff9f3170dea09d3a124a7cab9ae7
Raphael Eguchi <egurapha@gmail.com>
2025-01-18 01:14:08 -0500

    added sigint handling for list_bookmarks

--- src/list_bookmarks.sh
+++ src/list_bookmarks.sh
@@ -5,7 +5,17 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source $SCRIPT_DIR/colors.sh
 source $SCRIPT_DIR/functions.sh
 
-assert_bookmarks_exist
+
+function handle_kill() {
+    clear_output
+    tput cnorm
+    return 1
+}
+
+trap 'handle_kill; return 1' SIGINT
+
+if ! assert_bookmarks_exist; then
+    exit 1
+fi
+
 show_bookmarks "List Bookmarks"
-# read -rsn1 input
-clear_output
